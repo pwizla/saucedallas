5138fa963ca80dfb1409e1cd796cc4d1c56cc38a
Pierre Wizla <pwizla@users.noreply.github.com>
2016-06-16 16:35:39 +0200

    Add click event handling

--- script.js
+++ script.js
@@ -6,4 +6,14 @@ $(document).ready( function() {
 		quote = '"' + json.contents.quotes[whichQuote].quote + '"';
 		$(".quote").html(quote);
 	});
+
+	$("#getQuote").click(function () {
+		$.getJSON("https://raw.githubusercontent.com/pwizla/fcc/master/intermediate_front_end_projects/random_quote_machine/quotes.json", function(json) {
+			console.log('length: ', json.contents.quotes.length);
+			var whichQuote = Math.floor(Math.random() * (json.contents.quotes.length));
+			console.log('whichQuote', whichQuote);
+			quote = '"' + json.contents.quotes[whichQuote].quote + '"';
+			$(".quote").html(quote);
+		});
+	});
 });
