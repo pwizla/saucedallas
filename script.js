1151efa04a1dbd55e379aad8e4b51de7b8036d19
Pierre Wizla <pwizla@users.noreply.github.com>
2016-06-16 17:08:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ $(document).ready( function() {
 	$("#getQuote").click(function () {
 		$.getJSON("https://raw.githubusercontent.com/pwizla/saucedallas/gh-pages/quotes.json", function(json) {
 			console.log('length: ', json.contents.quotes.length);
-			var whichQuote = Math.floor(Math.random() * (json.contents.quotes.length));
+			whichQuote = Math.floor(Math.random() * (json.contents.quotes.length));
 			console.log('whichQuote', whichQuote);
 			quote = '"' + json.contents.quotes[whichQuote].quote + '"';
 			$(".quote").html(quote);
