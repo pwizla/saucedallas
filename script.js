b8cfa997dea490ee0f4f917060411f8e4576232d
pwizla <pwizla+github@gmail.com>
2016-06-16 15:24:12 +0200

    Import all files

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready( function() {
+	$.getJSON("https://raw.githubusercontent.com/pwizla/fcc/master/intermediate_front_end_projects/random_quote_machine/quotes.json", function(json) {
+		console.log('length: ', json.contents.quotes.length);
+		var whichQuote = Math.floor(Math.random() * (json.contents.quotes.length));
+		console.log('whichQuote', whichQuote);
+		quote = '"' + json.contents.quotes[whichQuote].quote + '"';
+		$(".quote").html(quote);
+	});
+});
