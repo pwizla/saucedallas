f22a6b742ab641471ee4df1ba05a21393ae32316
Pierre Wizla <pwizla@users.noreply.github.com>
2016-06-16 16:51:08 +0200

    Update quotes json file path

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready( function() {
-	$.getJSON("https://raw.githubusercontent.com/pwizla/fcc/master/intermediate_front_end_projects/random_quote_machine/quotes.json", function(json) {
+	$.getJSON("https://raw.githubusercontent.com/pwizla/saucedallas/gh-pages/quotes.json", function(json) {
 		console.log('length: ', json.contents.quotes.length);
 		var whichQuote = Math.floor(Math.random() * (json.contents.quotes.length));
 		console.log('whichQuote', whichQuote);
