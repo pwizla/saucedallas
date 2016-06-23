308195fd0f65c89cdbc25374e5e09e9bd08bc26a
Pierre Wizla <pwizla@users.noreply.github.com>
2016-06-23 14:25:25 +0200

    Remove console.log test message

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 $(document).ready( function() {
-	console.log("READY !");
 	$.getJSON("https://raw.githubusercontent.com/pwizla/saucedallas/gh-pages/quotes.json", function(json) {
 		console.log('How many quotes in the json file? ', json.contents.quotes.length);
 		var whichQuote = Math.floor(Math.random() * (json.contents.quotes.length));
